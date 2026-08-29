29ba0c0b636ba3d4ea9140d6ddf4ce8d3473adc4
Taufiq <fajarrahmatulloh59@gmail.com>
2026-08-29 12:54:50 +0700

    update 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return { base: '/template-wedding-01/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
